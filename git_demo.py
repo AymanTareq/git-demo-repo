fd96e301c4ac569c00b1ae3147ed1e7d1c11107a
AymanTareq.97 <tareq.ctec02@gmail.com>
2020-10-30 09:23:28 +0600

    remove some function and add br4 function

--- git_demo.py
+++ git_demo.py
@@ -1,14 +1,7 @@
 print("Hey, I'm learning GitHub.It's Awesome.")
 
-def greeting(name):
-    print('Welcome',name,'!')
-
-greeting('Ayman Tareq')
-
-def anotherf(name,age):
-    print(f"{name} is {age} years old.")
-
-anotherf('Tareq',23)
+def br4():
+    print('This function came from branch04')
 
 def newf():
     print('Hello, Ayman Tareq!')
