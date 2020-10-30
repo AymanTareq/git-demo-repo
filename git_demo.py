c51f72f97c7ff566b30a0ee4aa31af6078f21807
AymanTareq.97 <tareq.ctec02@gmail.com>
2020-10-30 09:05:45 +0600

    remove a function a add newf function

--- git_demo.py
+++ git_demo.py
@@ -5,7 +5,5 @@ def greeting(name):
 
 greeting('Ayman Tareq')
 
-def anotherf(name,age):
-    print(f"{name} is {age} years old.")
-
-anotherf('Tareq',23)
+def newf():
+    print('Hello, Ayman Tareq!')
